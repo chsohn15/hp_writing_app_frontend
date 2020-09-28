cf98a4bc913c7ff342296b8393aae533d3c7e7f5
chsohn15 <chsohn15@gmail.com>
2020-09-27 23:18:04 -0400

    fix merge

--- src/components/sortingHat/SortResult.js
+++ src/components/sortingHat/SortResult.js
@@ -1,11 +1,17 @@
-import React from 'react';
-import CharacterPicker from './CharacterPicker'
+import React from "react";
+import CharacterPicker from "./CharacterPicker";
 
 const SortResult = (props) => {
-    return ( <div>
-        You have been sorted into House {props.house}!
-        <CharacterPicker  house={props.house} houseCharacters={props.houseCharacters} setAlterEgo={props.setAlterEgo}/>
-    </div> );
-}
- 
+  return (
+    <div>
+      You have been sorted into House {props.house}!
+      <CharacterPicker
+        house={props.house}
+        houseCharacters={props.houseCharacters}
+        setAlterEgo={props.setAlterEgo}
+      />
+    </div>
+  );
+};
+
 export default SortResult;
