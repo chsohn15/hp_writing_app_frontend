8ae256994a8939c87c9b1d68723286c332ba8150
chsohn15 <chsohn15@gmail.com>
2020-09-30 22:58:13 -0400

    styled welcome page

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -14,17 +14,19 @@ const Welcome = (props) => {
 
     return(
         <div className="welcome">
-        <h1 style={{"color": "#F8F8F8"}}>Welcome to the</h1>
-        <img
-          style={{
-            width: "30%",
-            justifyContent: "center",
-          }}
-          src="https://www.logolynx.com/images/logolynx/47/4718783b27b71677a205ebed08d2cf4c.png"
-        />
-        <h1 style={{"color": "#F8F8F8"}}>Writing App!</h1>
-        <button onClick={login}>Login</button>
-        <button onClick={signup}>Sign Up</button>
+        <h1 id="welcome-header"style={{"color": "#E8E8E8"}}>The Official</h1>
+          <div className="welcome-text">
+            <img
+              style={{
+              width: "30%",
+              justifyContent: "center",
+            }}
+            src="https://www.logolynx.com/images/logolynx/47/4718783b27b71677a205ebed08d2cf4c.png"
+          />
+          <h1 style={{"color": "#F8F8F8"}}>Writing App!</h1>
+          <button onClick={login}>Login</button>
+          <button onClick={signup}>Sign Up</button>
+          </div>
         </div>
     )
 
